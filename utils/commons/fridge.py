c406154709cb861c1eddcb7a34dd9bfe899e2872
= <carlos.carballido@alumnos.uvigo.es>
2023-11-17 23:23:44 +0100

    Refactor: Thing class is an abstract method

--- utils/commons/fridge.py
+++ utils/commons/fridge.py
@@ -1,5 +1,5 @@
 from thing import Thing
-
+from beer import Beer
 class fridge(Thing):
     
     def __init__(self, literal_name, interactive, collision) -> None:
@@ -7,3 +7,8 @@ class fridge(Thing):
         opaque = True
         stackable = False
         container = True
+        beers = []
+        
+    def addBeer(self, nBeers: int):
+        for i in range(nBeers):
+            self.beers.append(Beer("Beer"))
