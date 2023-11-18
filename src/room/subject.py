51ab19483375e66024dea598d74480cef4373f87
= <carlos.carballido@alumnos.uvigo.es>
2023-11-18 20:11:21 +0100

    Add Observer and Observable classes

--- src/room/subject.py
+++ src/room/subject.py
@@ -1,4 +1,5 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from observer import Observer, Observable
 
 class Subject(ABC):
 	
@@ -6,50 +7,57 @@ class Subject(ABC):
     elements_list = []    # Lista de variables / atributos
     relations = {}        # Diccionario de relaciones entre ambos
 
+    @abstractmethod
+    def subscribe_to(self):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
     
-    def addObs(self,observer):
+    def addObs(self,observer:Observer) -> None:
         self.observers_list.append(observer)
 
     
-    def removeObs(self,observer):
+    def removeObs(self,observer:Observer) -> None:
         self.observers_list.remove(observer)
 
     
-    def addElem(self, element):
+    def addElem(self, element) -> None:
         self.elements_list.append(element)
 
     
-    def removeElem(self, element):
+    def removeElem(self, element) -> None:
          self.elements_list.remove(element)
 
     
-    def getElements(self):
+    def getElements(self) -> list:
         return self.elements_list
     
     
-    def getObservers(self):
+    def getObservers(self) -> list:
         return self.observers_list
 
 
     
-    def notify_all_observers(self):
+    def notify_all_observers(self) -> None:
         for observer in self.observers_list:
             observer.update(self)
 
     
-    def notify_interested_observers(self, element):
+    def notify_interested_observers(self, element) -> None:
         list_to_update = self.relations.get(element)
 
         for observer in list_to_update:
             observer.update()
 
     
-    def update(self):
+    def update(self) -> None:
         pass
 
 
     
-    def purge_subject(self):
+    def purge_subject(self) -> None:
         dict_to_check = dict(zip(self.elements_list, self.observers_list))
 
         # Initialize lists to store valid keys and values
@@ -66,7 +74,7 @@ class Subject(ABC):
         self.elements_list = valid_elements.copy()
 
     
-    def observer_init(self, observer, topics_of_interest):
+    def observer_init(self, observer, topics_of_interest) -> None:
         self.addObs(observer)
         for topic in topics_of_interest:
             if topic not in self.relations.keys():
