e3f4ff682a1b69bcd641a80726ad18c1ff8d054c
David Asensio <davidasensio95@gmail.com>
2023-11-18 12:28:57 +0100

    Añado Subject y Observer por primera vez

---
+++ src/room/Observer.py
@@ -0,0 +1,10 @@
+import Subject
+
+
+class Object(Subject):
+
+    def __init__(self, list_of_topics):
+        topics_of_interest = list_of_topics.copy()
+
+    def update(self):
+        pass
--- src/room/Subject.py
+++ src/room/Subject.py
@@ -1,44 +1,80 @@
 from abc import ABCMeta, abstractmethod
 
 class Subject(metaclass=ABCMeta):
-    '''We define a 'template' for our classes using an abstract class.
-    All classes inheriting from Subject must implement it's methods.'''
 	
     @abstractmethod
     def __init__(self):
-        ...
+        self.observers_list = []  # Lista de observadores
+        self.elements_list = []    # Lista de variables / atributos
+        self.relations = {}        #Diccionario de relaciones entre ambos
 
     @abstractmethod
-    def __init__(self) -> None:
-        ...
+    def addObs(self,observer):
+        self.observers_list.append(observer)
 
     @abstractmethod
-    def addObserver(self):
-        ...
+    def removeObs(self,observer):
+        self.observers_list.remove(observer)
 
     @abstractmethod
-    def removeObserver(self):
-        ...
+    def addElem(self, element):
+        self.elements_list.append(element)
 
     @abstractmethod
-    def addElem(self):
-        ...
+    def removeElem(self, element):
+         self.elements_list.remove(element)
 
     @abstractmethod
-    def removeElem(self):
-        ...
+    def getElements(self):
+        return self.elements_list
+    
+    @abstractmethod
+    def getObservers(self):
+        return self.observers_list
+
 
     @abstractmethod
-    def notifyObservers(self):
-        ...
+    def notify_all_observers(self):
+        for observer in self.observers_list:
+            observer.update(self)
 
     @abstractmethod
-    def getSujetos(self):
-        ...
+    def notify_interested_observers(self, element):
+        list_to_update = self.relations.get(element)
+
+        for observer in list_to_update:
+            observer.update()
 
     @abstractmethod
-    def setSujetos(self):
-        ...
+    def update():
+        
 
-	
-		
+
+    @abstractmethod
+    def purge_subject(self):
+        dict_to_check = dict(zip(self.elements_list, self.observers_list))
+
+        # Initialize lists to store valid keys and values
+        valid_elements = []
+        valid_observers = []
+
+        # Iterate through the keys and check if they exist in the dictionary
+        for key in self.elements_list:
+            if key in dict_to_check:
+                valid_elements.append(key)
+                valid_observers.append(dict_to_check[key])
+
+        self.observers_list = valid_observers.copy()
+        self.elements_list = valid_elements.copy()
+
+    @abstractmethod
+    def observer_init(self, observer):
+        self.addObs(observer)
+        for topic in observer.topics_of_interest:
+            if topic not in self.relations.keys():
+                self.addElem(topic)
+                self.relations[topic] = [observer]
+            else:
+                self.relations[topic] = 
+        
+                
