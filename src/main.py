43d50d6d246f2e0329bb53d1af4fa288a5ab534c
= <carlos.carballido@alumnos.uvigo.es>
2023-11-19 14:31:54 +0100

    Add new menu to select the house dimensions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
-from src.mvc import model, view, observer, subject
+import os
+os.environ['PYTHONPATH'] = '.'
+
 import unittest
+from src.mvc import model, view
 from tests.test_room.test_model.initialization_test import TestRoomInitialization
 from tests.test_room.test_model.populate_test import TestPopulateRoom
 from tests.test_Objects.test_fridge import TestFridge
@@ -8,7 +11,27 @@ from tests.test_Objects.test_table import TestTable
 from tests.test_Objects.test_thing import TestThing
 from tests.test_Objects.test_wall import TestWall
 
-file_path = 'assets/default_10x10_room.json' #Filepath para los tests y la ejecucion posterior
+opcion = 0
+while opcion != 1 and opcion != 2 and opcion != 3:
+    opcion = int(input("Ingrese el tamaño de la casa \n1.10x10 \n2.16x10 \n3.16x16 \n--> "))
+    
+    if opcion == 1:
+        X_MATRIX = 10
+        Y_MATRIX = 10
+    elif opcion == 2:
+        X_MATRIX = 16
+        Y_MATRIX = 10
+    elif opcion == 3:
+        X_MATRIX = 16
+        Y_MATRIX = 16
+    else:
+        print("Opción no válida. Por favor, elija 1, 2 o 3.")
+
+# Constants:
+HEIGHT = 40 * Y_MATRIX
+WIDTH = 40 * X_MATRIX
+
+file_path = f'assets/default_{X_MATRIX}x{Y_MATRIX}_room.json' #Filepath para los tests y la ejecucion posterior
 
 #Se ejecutan todos los tests de objetos
 
@@ -27,15 +50,6 @@ model_tests_suite.addTest(initialization_instance_test)
 
 
 unittest.TextTestRunner(verbosity=2).run(objects_tests_suite)
-
-
-
-
-# Constants:
-X_MATRIX = 16
-Y_MATRIX = 16
-HEIGHT = 40 * Y_MATRIX
-WIDTH = 40 * X_MATRIX
 
 '''# Crear un sujeto
 subject = Subject()
