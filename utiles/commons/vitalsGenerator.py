f3c51b28bec91329e016ed1bb1f3aba82a0ff354
David Asensio <davidasensio95@gmail.com>
2023-12-14 00:40:12 +0100

    Add health monitor module and remove unnecessary imports

--- utiles/commons/vitalsGenerator.py
+++ utiles/commons/vitalsGenerator.py
@@ -1,15 +1,10 @@
 import sys
 sys.path.insert(0, '.')
 
-import tkinter as tk
-from tkinter import ttk
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
-import matplotlib.animation as animation
+
 import random
 import numpy as np
-import itertools
+
 
 global heart_rate
 global blood_pressure
