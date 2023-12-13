42c909068ee71cdef97e5f2eff54e9069549c408
David Asensio <davidasensio95@gmail.com>
2023-12-13 19:58:42 +0100

    Add Mock import to test_healthMonitor.py

--- utiles/commons/test_healthMonitor.py
+++ utiles/commons/test_healthMonitor.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch,Mock
 from utiles.commons.healthMonitor import update_vital
 
 class TestHealthMonitor(unittest.TestCase):
