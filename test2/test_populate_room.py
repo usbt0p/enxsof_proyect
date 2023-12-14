08e09ac1f840aa51620d3b290519efecbc694d0b
Carlos Carballido <carlos.carballido@alumnos.uvigo.es>
2023-12-14 18:56:39 +0100

    FIX: test_populate_room to include room layout and agent generation

--- test2/test_populate_room.py
+++ test2/test_populate_room.py
@@ -1,18 +1,54 @@
 import unittest
 from unittest.mock import patch
-
-from mvc.model import Model
+import utiles.commons.agent as agent
+from src.mvc.model import Model
 
 
 class TestModel(unittest.TestCase):
     def setUp(self):
         self.model = Model(16, 16)
 
-    @patch('builtins.input', side_effect=["assets/default_16x16_room.json"])
-    def test_populate_room(self, mock_input):
-        self.model.populate_room("assets/default_16x16_room.json")
-        # TODO Agrega aquí tus aserciones para verificar que la matriz de la habitación se haya poblado correctamente
+    def test_populate_room(self):
         
+        room_layout = [
+            "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall",
+            "Wall", 0, 0, 0, "Wall", 0, 0, 0, 0, 0, "Wall", 0, "Sofa", "Sofa", 0, "Wall",
+            "Wall", 0, "Sofa", 0, "Wall", 0, 0, 0, 0, 0, "Wall", 0, "Table", "Table", 0, "Wall",
+            "Wall", 0, "Table", 0, "Wall", 0, 0, 0, 0, 0, "Door", 0, 0, 0, 0, "Wall",
+            "Wall", 0, 0, 0, "Wall", 0, 0, "Table", 0, 0, "Wall", 0, 0, 0, 0, "Wall",
+            "Wall", 0, 0, 0, "Wall", 0, 0, 0, 0, 0, "Wall", 0, 0, 0, 0, "Wall",
+            "Wall", "Wall", "Door", "Wall", "Wall", 0, 0, 0, 0, 0, "Wall", 0, 0, 0, 0, "Wall",
+            "Wall", 0, 0, 0, 0, 0, 0, 0, 0, 0, "Wall", 0, 0, 0, 0, "Wall",
+            "Wall", 0, 0, 0, 0, 0, 0, 0, 0, 0, "Wall", 0, 0, 0, 0, "Wall",
+            "Wall", "Wall", "Wall", "Door", "Wall", "Wall", "Wall", "Wall", "Wall", 0, "Wall", 0, "Sofa", "Sofa", 0, "Wall",
+            "Wall", 0, 0, 0, 0, 0, "Fridge", "Fridge", "Wall", 0, "Wall", 0, "Table", "Table", 0, "Wall",
+            "Wall", 0, 0, 0, 0, 0, 0, 0, "Wall", 0, "Wall", 0, "Sofa", "Sofa", 0, "Wall",
+            "Wall", 0, 0, 0, 0, 0, 0, 0, "Door", 0, "Door", 0, 0, 0, 0, "Wall",
+            "Wall", "Fridge", 0, 0, 0, 0, 0, 0, "Wall", 0, "Wall", 0, 0, 0, 0, "Wall",
+            "Wall", "Fridge", 0, "Table", "Table", 0, 0, 0, "Wall", 0, "Wall", 0, 0, 0, 0, "Wall",
+            "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall", "Wall",
+        ]
+
+        aux = []
+        
+        room = Model(16, 16)
+    
+        file_path = 'assets/default_16x16_room.json'
+
+        room.populate_room(file_path)
+
+        gato = agent.Agent("Gato", 7, 7)
+        room.generate_agents(gato)
+
+        for row in room.matrix:
+            for element in row:
+                if element == 0:
+                    aux.append(0)
+                else:
+                    aux.append(element._literal_name)
+                    
+        self.assertEqual(aux, room_layout)
+                
     def tearDown(self):
         # Realiza la limpieza de recursos si es necesario
         pass
